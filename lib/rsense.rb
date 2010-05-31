e6092966319eca3cdd18d933a293152412eb6cce
Sam Clopton <samsinite@gmail.com>
2010-05-30 22:34:39 -0700

    type inference

--- lib/rsense.rb
+++ lib/rsense.rb
@@ -12,7 +12,9 @@ module Redcar
         sub_menu "Plugins" do
           sub_menu "RSense" do
             item "code completion", RSense::CodeCompleteCommand
-            item "key binding", RSense::ChangeKeyComboCommand
+            item "type inference", RSense::TypeInferenceCommand
+            item "code completion key binding", RSense::ChangeCompletionKeyComboCommand
+            item "type inference key binding", RSense::ChangeInferenceKeyComboCommand
           end
         end
       end
@@ -42,17 +44,35 @@ module Redcar
       @storage ||= Plugin::Storage.new('rsense_plugin')
     end
     
-    def self.key_combo      
-      RSense.storage["key_combo"] || "Ctrl+/"              
+    def self.inference_key_combo      
+      RSense.storage["inference_key_combo"] || "Ctrl+Shift+/"              
     end
     
-    def self.key_combo=(key)
+    def self.inference_key_combo=(key)
       old_key = key_combo
       old_key_string = Redcar::ApplicationSWT::Menu::BindingTranslator.platform_key_string(old_key)
       item = Redcar::ApplicationSWT::Menu.items[old_key_string]
       Redcar::ApplicationSWT::Menu.items.delete(old_key_string)
       Redcar.app.main_keymap.map.delete(old_key)
-      RSense.storage["key_combo"] = key
+      RSense.storage["inference_key_combo"] = key
+      Redcar.app.main_keymap.map[key] = RSense::TypeInferenceCommand
+      key_string = Redcar::ApplicationSWT::Menu::BindingTranslator.platform_key_string(key)
+      item.first.text = item.first.text.split("\t").first + "\t" + key_string
+      item.first.set_accelerator(Redcar::ApplicationSWT::Menu::BindingTranslator.key(key_string))
+      Redcar::ApplicationSWT::Menu.items[key_string] = item
+    end
+    
+    def self.completion_key_combo      
+      RSense.storage["completion_key_combo"] || "Ctrl+/"              
+    end
+    
+    def self.completion_key_combo=(key)
+      old_key = key_combo
+      old_key_string = Redcar::ApplicationSWT::Menu::BindingTranslator.platform_key_string(old_key)
+      item = Redcar::ApplicationSWT::Menu.items[old_key_string]
+      Redcar::ApplicationSWT::Menu.items.delete(old_key_string)
+      Redcar.app.main_keymap.map.delete(old_key)
+      RSense.storage["completion_key_combo"] = key
       Redcar.app.main_keymap.map[key] = RSense::CodeCompleteCommand
       key_string = Redcar::ApplicationSWT::Menu::BindingTranslator.platform_key_string(key)
       item.first.text = item.first.text.split("\t").first + "\t" + key_string
@@ -62,26 +82,125 @@ module Redcar
     
     def self.keymaps
       linwin = Keymap.build("main", [:linux, :windows]) do
-        link Redcar::RSense.key_combo, RSense::CodeCompleteCommand
+        link Redcar::RSense.completion_key_combo, RSense::CodeCompleteCommand
+        link Redcar::RSense.inference_key_combo, RSense::TypeInferenceCommand
       end
 
       osx = Keymap.build("main", :osx) do
-        link Redcar::RSense.key_combo, RSense::CodeCompleteCommand
+        link Redcar::RSense.completion_key_combo, RSense::CodeCompleteCommand
+        link Redcar::RSense.inference_key_combo, RSense::TypeInferenceCommand
       end
 
       [linwin, osx]
     end
     
-    class ChangeKeyComboCommand < Command
+    class ChangeCompletionKeyComboCommand < Command
       def execute
-        result = Application::Dialog.input("Key Combination", "Please enter new key combo (i.e. 'Ctrl+Shift+C')", Redcar::RSense.key_combo) do |text|
+        result = Application::Dialog.input("Code Completion Key Combination", "Please enter new key combo (i.e. 'Ctrl+Shift+C')", Redcar::RSense.completion_key_combo) do |text|
           unless text == ""
             nil
           else
             "invalid combination"
           end
       	end
-        Redcar::RSense.key_combo = result[:value] if result[:button ] == :ok
+        Redcar::RSense.completion_key_combo = result[:value] if result[:button ] == :ok
+      end
+    end
+    
+    class ChangeInferenceKeyComboCommand < Command
+      def execute
+        result = Application::Dialog.input("Inference Key Combination", "Please enter new key combo (i.e. 'Ctrl+Shift+C')", Redcar::RSense.inference_key_combo) do |text|
+          unless text == ""
+            nil
+          else
+            "invalid combination"
+          end
+      	end
+        Redcar::RSense.inference_key_combo = result[:value] if result[:button ] == :ok
+      end
+    end
+    
+    class TypeInferenceCommand < EditTabCommand
+      def execute
+        return unless doc.mirror
+        path = doc.mirror.path.split(/\/|\\/)
+        if (path.last.split(".").last =~ /rb|erb/) || path.last.split(".").length == 1
+          path[path.length-1]= path.last + "~"
+          path = path.join("/")
+          cursor_line_number = doc.cursor_line
+          cursor_line_str = doc.get_line(cursor_line_number)
+          cursor_line_offset = doc.cursor_line_offset
+          cursor_offset = doc.cursor_offset
+          cursor_line_end_offset = doc.cursor_line_end_offset
+          line_str = cursor_line_str.rstrip
+          new_line_length = cursor_line_str.length - line_str.length
+          line_end_length = line_str.length - cursor_line_offset
+          line_str = line_str[0..(cursor_line_offset-1)]          
+          line_split = line_str.split(/::|\./)        
+          prefix = ""
+          prefix = line_split.last unless line_str[line_str.length-1].chr =~ /:|\./
+          prefix_start_offset = doc.cursor_line_offset - prefix.length
+          
+          log("line_str: #{line_str} length: #{line_str.length}")          
+          log("line_end_length: #{line_end_length}")
+          log("prefix: #{prefix} length: #{prefix.length}")
+          log("prefix_start_offset: #{prefix_start_offset}")
+          log("cursor_offset: #{doc.cursor_offset}")
+          log("cursor_line_offset: #{doc.cursor_line_offset}")
+          log("cursor_line_end_offset: #{doc.cursor_line_end_offset}")
+          log("new_line_length: #{new_line_length}")
+          doc_str = doc.to_s[0..(cursor_offset-prefix.length-1)] + doc.to_s[(cursor_offset)..(doc.to_s.length-1)]
+          log("deleted_section: #{doc.to_s[(cursor_offset-prefix.length)..(cursor_line_end_offset-new_line_length-1)]}")
+
+          File.open(path, "wb") {|f| f.print doc_str }
+          type = get_type(path, prefix, prefix_start_offset)
+          if type
+            window = Redcar.app.focussed_window
+            location = window.focussed_notebook.focussed_tab.controller.edit_view.mate_text.viewer.getTextWidget.getLocationAtOffset(window.focussed_notebook.focussed_tab.controller.edit_view.cursor_offset)
+            absolute_x = location.x
+            absolute_y = location.y
+            location = window.focussed_notebook.focussed_tab.controller.edit_view.mate_text.viewer.getTextWidget.toDisplay(0,0)
+            absolute_x += location.x
+            absolute_y += location.y
+
+            tool_tip = Redcar::Application::Dialog.tool_tip(type)
+            tool_tip.set_location(absolute_x, absolute_y)
+          end
+          FileUtils.rm(path)
+        end
+      end
+      
+      def get_type(temp_path, prefix, offset_at_line)
+        line_offset = doc.cursor_line
+        words = []
+        project = Redcar::Project.window_projects[Redcar.app.focussed_window].path + "/" if Redcar::Project.window_projects[Redcar.app.focussed_window]
+        if project
+          command = "type-inference '--file=#{temp_path}' '--location=#{line_offset+1}:#{offset_at_line}' '--prefix=#{prefix}' '--detect-project=#{project}'"
+        else
+          command = "type-inference '--file=#{temp_path}' '--location=#{line_offset+1}:#{offset_at_line}' '--prefix=#{prefix}'"
+        end
+        
+        log("command: #{command}")
+        #result = `#{command}`        
+        RSense.rsense_io.puts(command)
+        result = ""
+        while l = RSense.rsense_io.gets and /^END/ !~ l          
+          result << l
+        end        
+        result << 'END'        
+        
+        result = result.split("\n")        
+        result.each do |item|
+          if item =~ /^type: /
+            log("item: #{item}")
+            return item.split(" ")[1]
+          end
+        end
+        nil
+      end
+
+      def log(message)
+        puts("==> RSense: #{message}")
       end
     end
 
