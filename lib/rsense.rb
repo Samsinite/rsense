6b7dab73522bfa0f53e2a6b0a093438d1b0d25bc
Sam Clopton <samsinite@gmail.com>
2010-06-17 08:50:52 -0700

    Added window listener

--- lib/rsense.rb
+++ lib/rsense.rb
@@ -3,6 +3,7 @@ require 'stringio'
 
 module Redcar
   class RSense
+    
     def self.menus
       unless File.exist?("#{Redcar.root}/plugins/rsense/.rsense")
         puts "no .rsense file detected, creating .rsense file"
@@ -37,7 +38,21 @@ module Redcar
     end
    
     def self.rsense_io
-      @io ||= IO.popen(primitive_command, 'r+')
+      if @io
+        return @io
+      else
+        @io_handler = Redcar.app.focussed_window.add_listener(:about_to_close) do |win|
+          Redcar::RSense.log("closing io")
+          @io.close if @io && !@io.closed?
+          Redcar::RSense.rsense_io = nil
+          Redcar::RSense.log("io closed")
+        end
+        @io = IO.popen(primitive_command, 'r+')
+      end
+    end
+    
+    def self.rsense_io=(val)
+      @io = val
     end
         
     def self.storage
@@ -80,6 +95,16 @@ module Redcar
       Redcar::ApplicationSWT::Menu.items[key_string] = item
     end
     
+    def self.run_command(command)
+      RSense.log("command: #{command}")      
+      rsense_io.puts(command)
+      result = ""
+      while l = rsense_io.gets and /^END/ !~ l          
+      result << l
+      end        
+      result << 'END'
+    end
+    
     def self.keymaps
       linwin = Keymap.build("main", [:linux, :windows]) do
         link Redcar::RSense.completion_key_combo, RSense::CodeCompleteCommand
@@ -92,6 +117,10 @@ module Redcar
       end
 
       [linwin, osx]
+    end
+    
+    def self.log(message)
+      puts("==> RSense: #{message}")
     end
     
     class ChangeCompletionKeyComboCommand < Command
@@ -141,30 +170,21 @@ module Redcar
           prefix = line_split.last unless line_str[line_str.length-1].chr =~ /:|\./
           prefix_start_offset = doc.cursor_line_offset - prefix.length
           
-          log("line_str: #{line_str} length: #{line_str.length}")          
-          log("line_end_length: #{line_end_length}")
-          log("prefix: #{prefix} length: #{prefix.length}")
-          log("prefix_start_offset: #{prefix_start_offset}")
-          log("cursor_offset: #{doc.cursor_offset}")
-          log("cursor_line_offset: #{doc.cursor_line_offset}")
-          log("cursor_line_end_offset: #{doc.cursor_line_end_offset}")
-          log("new_line_length: #{new_line_length}")
+          RSense.log("line_str: #{line_str} length: #{line_str.length}")          
+          RSense.log("line_end_length: #{line_end_length}")
+          RSense.log("prefix: #{prefix} length: #{prefix.length}")
+          RSense.log("prefix_start_offset: #{prefix_start_offset}")
+          RSense.log("cursor_offset: #{doc.cursor_offset}")
+          RSense.log("cursor_line_offset: #{doc.cursor_line_offset}")
+          RSense.log("cursor_line_end_offset: #{doc.cursor_line_end_offset}")
+          RSense.log("new_line_length: #{new_line_length}")
           doc_str = doc.to_s[0..(cursor_offset-prefix.length-1)] + doc.to_s[(cursor_offset)..(doc.to_s.length-1)]
-          log("deleted_section: #{doc.to_s[(cursor_offset-prefix.length)..(cursor_line_end_offset-new_line_length-1)]}")
+          RSense.log("deleted_section: #{doc.to_s[(cursor_offset-prefix.length)..(cursor_line_end_offset-new_line_length-1)]}")
 
           File.open(path, "wb") {|f| f.print doc_str }
           type = get_type(path, prefix, prefix_start_offset)
-          if type
-            window = Redcar.app.focussed_window
-            location = window.focussed_notebook.focussed_tab.controller.edit_view.mate_text.viewer.getTextWidget.getLocationAtOffset(window.focussed_notebook.focussed_tab.controller.edit_view.cursor_offset)
-            absolute_x = location.x
-            absolute_y = location.y
-            location = window.focussed_notebook.focussed_tab.controller.edit_view.mate_text.viewer.getTextWidget.toDisplay(0,0)
-            absolute_x += location.x
-            absolute_y += location.y
-
-            tool_tip = Redcar::Application::Dialog.tool_tip(type)
-            tool_tip.set_location(absolute_x, absolute_y)
+          if type            
+            tool_tip = Redcar::Application::Dialog.tool_tip(type, :cursor)
           end
           FileUtils.rm(path)
         end
@@ -179,29 +199,18 @@ module Redcar
         else
           command = "type-inference '--file=#{temp_path}' '--location=#{line_offset+1}:#{offset_at_line}' '--prefix=#{prefix}'"
         end
-        
-        log("command: #{command}")
-        #result = `#{command}`        
-        RSense.rsense_io.puts(command)
-        result = ""
-        while l = RSense.rsense_io.gets and /^END/ !~ l          
-          result << l
-        end        
-        result << 'END'        
+                
+        result = RSense.run_command(command)        
         
         result = result.split("\n")        
         result.each do |item|
           if item =~ /^type: /
-            log("item: #{item}")
+            RSense.log("item: #{item}")
             return item.split(" ")[1]
           end
         end
         nil
-      end
-
-      def log(message)
-        puts("==> RSense: #{message}")
-      end
+      end      
     end
 
     class CodeCompleteCommand < EditTabCommand      
@@ -226,16 +235,16 @@ module Redcar
           prefix = line_split.last unless line_str[line_str.length-1].chr =~ /:|\./
           prefix_start_offset = doc.cursor_line_offset - prefix.length
           
-          log("line_str: #{line_str} length: #{line_str.length}")          
-          log("line_end_length: #{line_end_length}")
-          log("prefix: #{prefix} length: #{prefix.length}")
-          log("prefix_start_offset: #{prefix_start_offset}")
-          log("cursor_offset: #{doc.cursor_offset}")
-          log("cursor_line_offset: #{doc.cursor_line_offset}")
-          log("cursor_line_end_offset: #{doc.cursor_line_end_offset}")
-          log("new_line_length: #{new_line_length}")
+          RSense.log("line_str: #{line_str} length: #{line_str.length}")          
+          RSense.log("line_end_length: #{line_end_length}")
+          RSense.log("prefix: #{prefix} length: #{prefix.length}")
+          RSense.log("prefix_start_offset: #{prefix_start_offset}")
+          RSense.log("cursor_offset: #{doc.cursor_offset}")
+          RSense.log("cursor_line_offset: #{doc.cursor_line_offset}")
+          RSense.log("cursor_line_end_offset: #{doc.cursor_line_end_offset}")
+          RSense.log("new_line_length: #{new_line_length}")
           doc_str = doc.to_s[0..(cursor_offset-prefix.length-1)] + doc.to_s[(cursor_offset)..(doc.to_s.length-1)]
-          log("deleted_section: #{doc.to_s[(cursor_offset-prefix.length)..(cursor_line_end_offset-new_line_length-1)]}")
+          RSense.log("deleted_section: #{doc.to_s[(cursor_offset-prefix.length)..(cursor_line_end_offset-new_line_length-1)]}")
 
           File.open(path, "wb") {|f| f.print doc_str }
           completions = get_completions(path, prefix, prefix_start_offset)
@@ -250,7 +259,7 @@ module Redcar
           end
           
           Redcar::Application::Dialog.popup_menu(builder.menu, :cursor)
-          FileUtils.rm(path)
+#          FileUtils.rm(path)
         end
       end
       
@@ -264,20 +273,13 @@ module Redcar
           command = "code-completion '--file=#{temp_path}' '--location=#{line_offset+1}:#{offset_at_line}' '--prefix=#{prefix}'"
         end
         
-        log("command: #{command}")
-        #result = `#{command}`        
-        RSense.rsense_io.puts(command)
-        result = ""
-        while l = RSense.rsense_io.gets and /^END/ !~ l          
-          result << l
-        end        
-        result << 'END'        
+        result = RSense.run_command(command)
         
         result = result.split("\n")
         completions = []
         result.each do |item|
           if item =~ /^completion: /
-            log("item: #{item}")
+            RSense.log("item: #{item}")
             item_a = item.split(" ")
             dict = {}
             dict[:word] = item_a[1]
@@ -292,10 +294,6 @@ module Redcar
           end
         end
         completions
-      end
-
-      def log(message)
-        puts("==> RSense: #{message}")
       end
     end
   end
