1ff0a5ba263b25e0a26175a2c938f1c7cd5755cd
Sam Clopton <samsinite@gmail.com>
2010-09-12 15:03:53 -0700

    Erik's cursor position patch

--- lib/rsense.rb
+++ lib/rsense.rb
@@ -254,6 +254,7 @@ module Redcar
             completions.each do |completion|
               item(completion[:word] + "\t" + completion[:kind]) do              
                 cur_doc.replace(cur_doc.cursor_offset - prefix.length, prefix.length, completion[:word])
+                cur_doc.cursor_offset = cur_doc.cursor_offset + scompletion[:word].length
               end
             end
           end
